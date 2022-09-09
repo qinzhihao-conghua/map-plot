e14392f79941255f850262e266ea746e893ab8e1
qinzhihao-conghua <1043419697@qq.com>
2022-09-09 09:32:08 +0800

    examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Map, View } from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import { transform } from 'ol/proj';
+import OSM from 'ol/source/OSM';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'map-plot';
+  map: Map;
+  ngAfterViewInit() {
+    this.map = new Map({
+      target: 'plotMap',
+      layers: [
+        new TileLayer({ source: new OSM() })
+      ],
+      view: new View({
+        // center: transform([108.316492, 22.818136], 'EPSG:4326', 'EPSG:3857'),
+        center: [108.316492, 22.818136],
+        zoom: 12,
+        maxZoom: 20,
+        minZoom: 6,
+        projection: 'EPSG:4326'
+      })
+    })
+  }
 }
